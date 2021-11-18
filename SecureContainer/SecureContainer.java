b78900708d4ef68526fc557e6e2c24beddf88a29
PeterVenhuizen <peter_venhuizen@hotmail.com>
2021-11-18 16:16:30 +0100

    Finish part 2

--- SecureContainer/SecureContainer.java
+++ SecureContainer/SecureContainer.java
@@ -1,9 +1,10 @@
 import java.util.*;
+import java.util.function.IntUnaryOperator;
 
 class PossiblePassword {
-    private int password;
-    private char[] pwCharArray;
-    private boolean valid;
+    protected int password;
+    protected char[] pwCharArray;
+    protected boolean valid;
 
     public PossiblePassword(int password) {
         this.password = password;
@@ -11,16 +12,16 @@ class PossiblePassword {
         this.valid = this.setPasswordValidity();
     }
 
-    private char[] convertPasswordToCharArray() {
+    protected char[] convertPasswordToCharArray() {
         String number = String.valueOf(this.password);
         return number.toCharArray();
     }
 
-    private boolean setPasswordValidity() {
+    protected boolean setPasswordValidity() {
         return this.twoAdjacentDigitsAreTheSame() && this.theDigitsNeverDecrease();
     }
 
-    private boolean twoAdjacentDigitsAreTheSame() {
+    protected boolean twoAdjacentDigitsAreTheSame() {
         for (int i = 0; i < this.pwCharArray.length - 1; i++) {
             if (this.pwCharArray[i] == this.pwCharArray[i+1]) {
                 return true;
@@ -29,7 +30,7 @@ class PossiblePassword {
         return false;
     }
 
-    private boolean theDigitsNeverDecrease() {
+    protected boolean theDigitsNeverDecrease() {
         for (int i = this.pwCharArray.length - 1; i > 0; i--) {
             if (this.pwCharArray[i] < this.pwCharArray[i-1]) {
                 return false;
@@ -43,22 +44,65 @@ class PossiblePassword {
     }
 }
 
-public class SecureContainer {
-    static private List<PossiblePassword> possiblePasswords = new ArrayList<>();
+class PossiblePasswordPart2 extends PossiblePassword {
 
-    public static int getNumberOfPossiblePasswords(int lowerBound, int upperBound) {
+    public PossiblePasswordPart2(int password) {
+        super(password);
+    }
+
+    private boolean adjacentMatchingDigtisAreNotPartOfALargerGroup() {
+        
+        int numberOfValidGroups = 0;
+
+        List<Character> matchingGroup = new ArrayList<Character>();
+        matchingGroup.add(this.pwCharArray[0]);
+
+        for (int i = 1; i < this.pwCharArray.length; i++) {
+            char nextChar = this.pwCharArray[i];
+            if (nextChar != matchingGroup.get(matchingGroup.size() - 1)) {
+                if (matchingGroup.size() == 2) {
+                    numberOfValidGroups++;
+                }
+                matchingGroup.clear();
+            }
+            matchingGroup.add(nextChar);
+        }
+
+        if (matchingGroup.size() == 2) {
+            numberOfValidGroups++;
+        }
+
+        return numberOfValidGroups >= 1;
+    }
+
+    @Override
+    protected boolean setPasswordValidity() {
+        return this.theDigitsNeverDecrease() 
+            && this.adjacentMatchingDigtisAreNotPartOfALargerGroup();
+    }
+}
+
+public class SecureContainer {
+
+    public static void getNumberOfPossiblePasswords(int lowerBound, int upperBound) {
+        int answerToPart1 = 0, answerToPart2 = 0;
+
         int password = lowerBound;
         while (password <= upperBound) {
 
             PossiblePassword pw = new PossiblePassword(password);
+            if (pw.isValid())
+                answerToPart1++;
 
-            if (pw.isValid()) {
-                possiblePasswords.add(pw);
-            }
+            PossiblePasswordPart2 pw2 = new PossiblePasswordPart2(password);
+            if (pw2.isValid())
+                answerToPart2++;
 
             password++;
         }
-        return possiblePasswords.size();
+        // return possiblePasswords.size();
+        System.out.println("Answer for part1: " + answerToPart1);
+        System.out.println("Answer for part2: " + answerToPart2);
     }
 
     public static void main(String[] args) {
@@ -69,7 +113,12 @@ public class SecureContainer {
         System.out.println(new PossiblePassword(123789).isValid());
 
         // solution 1
-        int numberOfPossiblePasswords = getNumberOfPossiblePasswords(145852, 616942);
-        System.out.println(numberOfPossiblePasswords);
+        getNumberOfPossiblePasswords(145852, 616942);
+
+        // part 2
+        // tests
+        System.out.println(new PossiblePasswordPart2(112233).isValid());
+        System.out.println(new PossiblePasswordPart2(123444).isValid());
+        System.out.println(new PossiblePasswordPart2(111122).isValid());
     }
 }
